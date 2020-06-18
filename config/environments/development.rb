e94640f2e7bce1e1e0e2cb058bb2ace8ab8cff5e
shun255454 <donp3461@gmail.com>
2020-06-18 17:52:07 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_cable.allowed_request_origins = [ 'http://54.168.10.0/' ]
-  config.web_console.whitelisted_ips = 'crient-server-address'
 end
